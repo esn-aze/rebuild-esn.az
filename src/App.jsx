91a2c75d12f9ed264e5e3ff9c1ef7691572cb0d8
Said Yolciyev <said14y2007@gmail.com>
2024-12-15 13:09:44 +0400

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 export const App = () => {
-  return <div className="container p-6">Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
